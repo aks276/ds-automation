84cab0a702ed5bf35ac46bae0c9c75feab6e8e4a
Aman K. Shihab <amanshihab273@gmail.com>
2022-11-05 10:37:47 +0530

    Added cells to save the model

--- ds-automation/models.py
+++ ds-automation/models.py
@@ -7,11 +7,8 @@ def get_models(config):
     if "type" not in config:
         raise ValueError("'type' not in config")
     cells = []
-    cells.append(
-        cell_creation.code_cell(
-            "import sklearn.model_selection\nimport sklearn.metrics"
-        )
-    )
+    cells.extend(data.get_data_cells(config=config))
+    cells.append(cell_creation.md_cell("#### Model Building"))
     if config["type"] == "classification":
         cells.append(cell_creation.code_cell("import autosklearn.classification"))
         cells.append(cell_creation.code_cell("automl = autosklearn.classification.AutoSklearnClassifier()"))
@@ -19,14 +16,22 @@ def get_models(config):
         cells.append(cell_creation.code_cell("import autosklearn.regression"))
         cells.append(cell_creation.code_cell("automl = autosklearn.regression.AutoSklearnRegressor()"))
     else:
-        NotImplementedError(f"{config['type']} not yet implemented.")
-    cells.extend(data.get_data_cells(config=config))
-    cells.append(cell_creation.md_cell("#### Model Building"))
+        raise NotImplementedError(f"{config['type']} not yet implemented.")
     cells.append(cell_creation.code_cell("automl.fit(X_train, y_train)"))
 
     cells.append(cell_creation.md_cell("Performance Measures"))
     cells.append(cell_creation.code_cell("from sklearn.metrics import accuracy_score"))
     cells.append(cell_creation.code_cell("y_hat = automl.predict(X_test)"))
     cells.append(cell_creation.code_cell("print('Accuracy score', accuracy_score(y_test, y_hat))"))
+    
+    cells.append(cell_creation.md_cell("##### Run Information of the models"))
+    cells.append(cell_creation.code_cell("print(automl.sprint_statistics())"))
+
+    cells.append(cell_creation.code_cell("import pprint"))
+    cells.append(cell_creation.code_cell("pprint(automl.show_models(), indent=4)"))
+
+    cells.append(cell_creation.md_cell("#### Saving the model"))
+    cells.append(cell_creation.code_cell("import pickle"))
+    cells.append(cell_creation.code_cell("pickle.dump(automl, open('saved_model.sav', 'wb'))"))
 
     return cells
